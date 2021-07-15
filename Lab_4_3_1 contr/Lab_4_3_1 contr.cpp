d7ea7551652a2a6f69cfa9eb214636511339abbb
auroraclub <alexraykov2000@gmail.com>
2021-07-15 19:42:53 +0300

    lab 4

---
+++ Lab_4_3_1 contr/Lab_4_3_1 contr.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_4_3_1 contr.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+int Myroot(double, double, double, double&, double&);
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+int Myroot(double a, double b, double c, double& x1, double& x2) 
+{
+
+    if ((b * b - 4 * a * c) >= 0) //Если дискриминант больше или равен 0
+    {
+        x1 = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
+        std::cout << "Первый корень равен " << x1 << std::endl;
+        x2 = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
+        std::cout << "Второй корень равен " << x2 << std::endl;
+
+        if ((b * b - 4 * a * c) == 0)
+        {
+            return 0;
+        }
+        return 1;
+    }
+    else 
+    {
+        return -1;
+    }
+}
---
+++ Lab_4_3_2 contr/Lab_4_3_2 contr.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+bool Input(int a, int b) 
+{
+	std::cout << "enter a:";
+	std::cin >> a;
+	std::cout << "enter b:";
+	std::cin >> b;
+	if ((a > 0 && a != NULL) && (b < 0 && b != NULL))
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	int a, b;
+	if (Input(a, b) == false) // if(!Input(a,b))
+	{
+		std::cerr << "error";
+		return 1;
+	}
+	int s = a + b;
+	return 0;
+}
