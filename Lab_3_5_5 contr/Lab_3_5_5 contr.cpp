d79daa58d75d632240ede444b830f4172705262c
auroraclub <alexraykov2000@gmail.com>
2021-07-15 18:58:58 +0300

    lab3 final

---
+++ Lab_3_5_5 contr/Lab_3_5_5 contr.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+void decToBin(int n) {
+    if (n < 0) {
+        cout << "-";
+        decToBin(-n);
+    }
+    else if (n < 2) {
+        cout << n;
+        return;
+    }
+    else {
+        decToBin(n / 2);
+        int res = n % 2;
+        cout << res;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter an integer number:\n";
+    cin >> n;
+    cout << "Target:\n";
+    decToBin(n);
+    cout << endl;
+    return 0;
+}
