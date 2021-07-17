aebc3f738798cd2025bd3de52023c7ee3a554c86
auroraclub <alexraykov2000@gmail.com>
2021-07-17 18:41:09 +0300

    lab_5

---
+++ Lab_5_1_1/Lab_5_1_1.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+
+int sum(int n, int* mas);
+int sumNegative(int n, int* mas);
+int sumPositive(int n, int* mas);
+int sumOdd(int n, int* mas);
+int sumEven(int n, int* mas);
+int indexOfMax(int n, int* mas);
+
+int main()
+{
+	const int n = 10;
+	int mas[n];
+	int s = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << "mas[" << i << "]=";
+		std::cin >> mas[i];
+	}
+	std::cout << "Sum = " << sum(n, mas);
+}
+
+int sum(int n, int* mas)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		s += mas[i];
+	}
+	return s;
+}
+
+int sumNegative(int n, int* mas)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (mas[i] < 0)
+		{
+			s += mas[i];
+		}
+	}
+	return s;
+}
+
+int sumPositive(int n, int* mas)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (mas[i] > 0)
+		{
+			s += mas[i];
+		}
+	}
+	return s;
+}
+
+int sumOdd(int n, int* mas)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 != 0)
+		{
+			s += mas[i];
+		}
+	}
+	return s;
+}
+
+int sumEven(int n, int* mas)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			s += mas[i];
+		}
+	}
+	return s;
+}
+
+int indexOfMax(int n, int* mas)
+{
+	int index = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (mas[i - 1] < mas[i])
+		{
+			index = i;
+		}
+	}
+	return index;
+}
+
+int indexOfMin(int n, int* mas)
+{
+	int index = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (mas[i - 1] > mas[i])
+		{
+			index = i;
+		}
+	}
+	return index;
+}
+
+
+int multiBetweenIndexes(int n, int* mas)
+{
+	int indexMax = indexOfMax(n, mas);
+	int indexMin = indexOfMin(n, mas);
+
+	if (indexMax == indexMin + 1 || indexMax + 1 == indexMin)
+	{
+		int multy = mas[indexMin] * mas[indexMax];
+		return multy;
+	}
+	if (indexMin < indexMax)
+	{
+		int multy = mas[indexMin];
+		for (int i = indexMin + 1; i < indexMax; i++)
+		{
+			multy *= mas[i];
+		}
+		return multy;
+	}
+	else 
+	{
+		int multy = mas[indexMin];
+		for (int i = indexMax + 1; i < indexMin; i++)
+		{
+			multy *= mas[i];
+		}
+		return multy;
+	}
+}
+
---
+++ Lab_5_2_1 contr/Lab_5_2_1 contr.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int* max_vect(int n, int* a, int* b);
+int max(int a, int b);
+
+int main()
+{
+	int a[] = { 1,2,3,4,5,6,7,2 };
+	int b[] = { 7,6,5,4,3,2,1,3 };
+	int kc = sizeof(a) / sizeof(a[0]); //Количество элементов массива
+	int* c; //Указатель на результирующий массив
+	c = max_vect(kc, a, b); //вызов функции для создания массива
+	for (int i = 0; i < kc; i++) //Вывод результата.
+		std::cout << c[i] << " ";
+	std::cout << std::endl;
+	delete[]c; //Возврат памяти.
+}
+
+int* max_vect(int n, int* a, int* b)
+{
+	int* res = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		res[i] = max(a[i], b[i]);
+	}
+	return res;
+}
+
+int max(int a, int b)
+{
+	int max;
+	if (a >= b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	return max;
+}
