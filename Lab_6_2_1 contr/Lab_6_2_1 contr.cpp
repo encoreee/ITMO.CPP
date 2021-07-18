f1251b6958b7b6e4bcff3051caaffd76c4ddfe75
auroraclub <alexraykov2000@gmail.com>
2021-07-18 23:31:46 +0300

    lab 6

---
+++ Lab_6_2_1 contr/Lab_6_2_1 contr.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ofstream out("D:\\poesy.txt");
+
+    if (!out.is_open())
+    {
+		std::cout << "Error\n";
+		return 1;
+    }
+    
+	std::string line = "init";
+	while (line != "")
+	{
+		std::cout << "Enter new line or empty to finish" << std::endl;
+		std::getline(std::cin, line);
+		out << line << std::endl;
+	}
+
+	out.close();
+}
