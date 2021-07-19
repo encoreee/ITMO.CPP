4c4687c1fe86393dc3db913ed4eaea4b09322c0c
auroraclub <alexraykov2000@gmail.com>
2021-07-20 01:09:30 +0300

    lab7

--- Lab_6_2_2/Lab_6_2_2.cpp
+++ Lab_6_2_2/Lab_6_2_2.cpp
@@ -1,26 +1,74 @@
-﻿
+﻿#include <string>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string convertToString(int* a, int size)
+{
+	std::string str;
+	for (size_t i = 0; i < size; i++)
+	{
+		str += std::to_string(a[i]);
+		str += " ";
+
+	}
+
+	return str;
+}
+
+int* sort(const int* a,  int size)
+{
+	int* mas = new int[size];
+	int min = 0; // для записи минимального значения
+	int buf = 0; // для обмена значениями
+
+	for (unsigned i = 0; i < size; i++)
+	{
+		mas[i] = a[i];
+
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		min = i; // номер текущей ячейки, как ячейки с минимальным значением
+		// в цикле найдем реальный номер ячейки с минимальным значением
+		for (int j = i + 1; j < size; j++)
+			min = (mas[j] < mas[min]) ? j : min;
+		// перестановка этого элемента, поменяв его местами с текущим
+		if (i != min)
+		{
+			buf = mas[i];
+			mas[i] = mas[min];
+			mas[min] = buf;
+		}
+	}
+	return mas;
+}
 
 int main()
 {
 	const int N = 10;
-	int a[N] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
-	int min = 0; // для записи минимального значения
-	int buf = 0; // для обмена значениями
+	int source[N] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
+	int* dest = sort(source, N);
+	
+	int a_size = sizeof(source) / sizeof(int);
 
-	for (int i = 0; i < N; i++)
+	std::ofstream out("D:\\array.txt");
+
+	if (!out.is_open())
 	{
-		min = i; // номер текущей ячейки, как ячейки с минимальным значением
-		// в цикле найдем реальный номер ячейки с минимальным значением
-		for (int j = i + 1; j < N; j++)
-			min = (a[j] < a[min]) ? j : min;
-		// перестановка этого элемента, поменяв его местами с текущим
-		if (i != min)
-		{
-			buf = a[i];
-			a[i] = a[min];
-			a[min] = buf;
-		}
+		std::cout << "Error\n";
+		return 1;
 	}
+
+	std::string line = convertToString(source, a_size);
+	out << line << std::endl;
+
+	dest = sort(source, N);
+
+	line = convertToString(dest, a_size);
+	out << line << std::endl;
+
+	out.close();
 }
 
---
+++ Lab_7_4_1 contr/Lab_7_4_1 contr.cpp
@@ -0,0 +1,168 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std;
+
+struct Time
+{
+	int hour;
+	int minutes;
+	int seconds;
+	char format[6]{}; // am/pm (12-hours), utc (24-hours)
+
+	Time();
+	Time(int hour, int minutes, int seconds, const char* format = "utc");
+	Time(const Time& obj);
+	~Time();
+	void showTime();
+
+	void setHour(int hour);
+	int getHour();
+	void setMinutes(int minutes);
+	int getMinutes();
+	void setSeconds(int seconds);
+	int getSeconds();
+	bool setFormat(const char* format);
+	const char* getFormat();
+
+	bool valid();
+	void tickTime();
+};
+
+
+void Time::tickTime()
+{
+	this->seconds += 1;
+
+	if (this->seconds >= 60)
+	{
+		this->seconds %= 60;
+		this->minutes += 1;
+	}
+
+	if (this->minutes >= 60)
+	{
+		this->minutes %= 60;
+		this->hour += 1;
+	}
+
+	if (this->hour >= 24)
+		this->minutes %= 24;
+}
+
+bool Time::valid()
+{
+	time_t t = time(NULL);
+	tm flag;
+	localtime_s(&flag, &t);
+
+	if (this->hour == flag.tm_hour && this->minutes == flag.tm_min && this->seconds == flag.tm_sec)
+		return true;
+	return false;
+}
+
+bool Time::setFormat(const char* format)
+{
+	if (strcmp("am/pm", format) == 0 || strcmp("utc", format) == 0)
+	{
+		strcpy_s(this->format, format);
+		return true;
+	}
+	return false;
+}
+
+const char* Time::getFormat()
+{
+	return this->format;
+}
+
+
+Time::Time()
+{
+	time_t t = time(NULL);
+	tm flag;
+	localtime_s(&flag, &t);
+
+	this->hour = flag.tm_hour;
+	this->minutes = flag.tm_min;
+	this->seconds = flag.tm_sec;
+
+	strcpy_s(this->format, "utc");
+}
+
+Time::Time(int hour, int minutes, int seconds, const char* format)
+{
+	this->hour = hour;
+	this->minutes = minutes;
+	this->seconds = seconds;
+	strcpy_s(this->format, "utc");
+}
+
+Time::Time(const Time& obj)
+{
+	this->hour = obj.hour;
+	this->minutes = obj.minutes;
+	this->seconds = obj.seconds;
+	strcpy_s(this->format, obj.format);
+}
+
+Time::~Time() {}
+
+void Time::showTime()
+{
+	if (strcmp("am/pm", format) == 0)
+	{
+		cout << "Текущее время = " << hour % 12 << " : " << minutes << " : " << seconds << " (";
+		if (hour > 12) cout << "pm)\n"; else cout << "am)\n";
+	}
+
+	else
+		cout << "Текущее время = " << hour << " : " << minutes << " : " << seconds << " (" << format << ")\n";
+}
+
+void Time::setHour(int hour)
+{
+	this->hour = hour;
+}
+
+int Time::getHour()
+{
+	return hour;
+}
+
+void Time::setMinutes(int minutes)
+{
+	this->minutes = minutes;
+}
+
+int Time::getMinutes()
+{
+	return minutes;
+}
+
+void Time::setSeconds(int seconds)
+{
+	this->seconds = seconds;
+}
+
+int Time::getSeconds()
+{
+	return seconds;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Time when;
+	when.showTime();
+
+	cout << when.getFormat() << endl;
+
+	cout << when.setFormat("am/pm") << endl;
+
+	cout << when.getFormat() << endl;
+
+	when.showTime();
+
+}
